23d19e98e3ee6bd7abb27eec2dd9893caafc854e
poorvarathore05 <poorvarathore05@gmail.com>
2021-11-08 18:02:51 -0800

    created a login/signup form

--- jobly/src/Login.js
+++ jobly/src/Login.js
@@ -1,9 +1,43 @@
+import { useState } from "react";
+import { Button, Form, FormGroup, Label } from 'reactstrap';
+import './LoginForm.css';
+
 function Login() {
 
+    const [loginForm, setLoginForm] = useState({
+        username: "",
+        password: ""
+    });
+    function handleChange(evt) {
+        const { name, value } = evt.target;
+        setLoginForm(data => ({
+            ...data,
+            [name]: value
+        }));
+    }
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        let { ...data } = loginForm;
+        console.log(data);
+        setLoginForm(data);
+    }
     return (
-        <>
-            <h3>Login</h3>
-        </>
+        <Form className="LoginForm" onSubmit={handleSubmit}>
+            <FormGroup ><Label htmlFor="username"> Username<small>*</small></Label>
+                <input placeholder="Username"
+                    name="username"
+                    type="text"
+                    value={loginForm.username}
+                    onChange={handleChange} />
+                <Label htmlFor="password">Password<small>*</small> </Label>
+                <input placeholder="Password"
+                    name="password"
+                    type="text"
+                    value={loginForm.password}
+                    onChange={handleChange} />
+            </FormGroup>
+            <Button className="Loginbtn" color="primary" size="lg">Login</Button>
+        </Form>
     )
 }
 
--- jobly/src/SignUp.js
+++ jobly/src/SignUp.js
@@ -1,11 +1,69 @@
 
+import { useState } from "react";
+import { Button, Card, Form, FormGroup, Label } from 'reactstrap';
+import './LoginForm.css'
 
 function SignUp() {
 
+    const [signUpForm, setSignUpForm] = useState({
+        username: "",
+        password: "",
+        email: "",
+        firstName: "",
+        lastName: ""
+    });
+    function handleChange(evt) {
+        const { name, value } = evt.target;
+        setSignUpForm(data => ({
+            ...data,
+            [name]: value
+        }));
+    }
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        let { ...data } = signUpForm;
+        console.log(data);
+        setSignUpForm(data);
+    }
     return (
-        <>
-            <h3>Signup</h3>
-        </>
+        <div className="LoginCard">
+            <div className="cardBody">
+                <form className="LoginForm" onSubmit={handleSubmit}>
+                    <Label htmlFor="username"> Username<small>*</small></Label>
+                    <input placeholder="Username"
+                        name="username"
+                        type="text"
+                        value={signUpForm.username}
+                        onChange={handleChange} />
+                    <Label htmlFor="password">Password<small>*</small> </Label>
+                    <input placeholder="Password"
+                        name="password"
+                        type="text"
+                        value={signUpForm.password}
+                        onChange={handleChange} />
+                    <Label htmlFor="email">Email<small>*</small> </Label>
+                    <input placeholder="Email"
+                        name="email"
+                        type="text"
+                        value={signUpForm.email}
+                        onChange={handleChange} />
+                    <Label htmlFor="firstName">FirstName<small>*</small> </Label>
+                    <input placeholder="FirstName"
+                        name="firstName"
+                        type="text"
+                        value={signUpForm.firstName}
+                        onChange={handleChange} />
+                    <Label htmlFor="lastName">LastName<small>*</small></Label>
+                    <input placeholder="LastName"
+                        name="lastName"
+                        type="text"
+                        value={signUpForm.lastName}
+                        onChange={handleChange} />
+
+                    <Button className="Loginbtn" color="primary" size="lg">SignUp</Button>
+                </form>
+            </div>
+        </div>
     )
 }
 
